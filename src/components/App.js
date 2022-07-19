5cf45d35fc108c403c65e280a03b895d8f9537e0
Nathália <nathalia.gramst@hotmail.com>
2022-07-19 14:38:40 -0300

    Arquivos iniciais

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <section class="welcome">
+            <img src="./assets/img/logo.png" alt="logo" />
+            <h1>ZapRecall</h1>
+            <button type="button">Iciniar Recall!</button>
+        </section>
+    )
+}
